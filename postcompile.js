38f513312d6ef248f28246ffa7271a1fabb0d4af
Xiongfei Shi <xiongfei.shi@icloud.com>
2024-02-27 09:59:42 +0800

    Update copyright

--- postcompile.js
+++ postcompile.js
@@ -1,3 +1,14 @@
+/*
+ * postcompile.js
+ *
+ * Copyright (c) 2024 Xiongfei Shi
+ *
+ * Author: Xiongfei Shi <xiongfei.shi(a)icloud.com>
+ * License: Apache-2.0
+ *
+ * https://github.com/shixiongfei/safety-socketio
+ */
+
 const fs = require("node:fs");
 
 fs.copyFileSync("./package.esm.json", "./lib/esm/package.json");
